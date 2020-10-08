f05866750b444dac1dadab463307433f8281c779
raymond <rsvvuuren@hotmail.com>
2020-10-08 22:40:20 +0200

    init

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Login;
+use App\Http\Requests\Register;
+use App\Models\User;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Session;
+
+class AuthController extends Controller
+{
+
+    public function register(Register $request)
+    {
+
+        $user = new User([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+
+        ]);
+        $user->save();
+        return response()->json(['ok'])->send();
+    }
+
+
+    public function login(Login $request)
+    {
+        $http = new Client();
+
+        $response = $http->post('http://web/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => '2',
+                'client_secret' => '2PfmZnJGgk1cwWA2uSVIdWhmr63FXOnkXHHpHYas',
+                'username' => $request->get('username'),
+                'password' => $request->get('password'),
+                'scope' => '',
+            ],
+        ]);
+
+        return json_decode((string)$response->getBody(), true);
+    }
+
+    public function profile(Request $request)
+    {
+        return response()->json($request->user())
+            ->header('X_OAUTH_CLIENT_ID', $request->get('X_OAUTH_CLIENT_ID'))
+            ->header('X_OAUTH_USER_ID', $request->get('X_OAUTH_USER_ID'))
+            ->send();
+    }
+}
